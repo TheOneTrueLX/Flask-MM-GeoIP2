425fe46807bd29e0af3997b81ab8a9139757666c
TheOneTrueLX <lx@t0xic.com>
2019-01-17 06:55:44 -0800

    Refactored lookup functions to separate webservice and local DB lookups

--- flask_mm_geoip2/__init__.py
+++ flask_mm_geoip2/__init__.py
@@ -3,12 +3,7 @@ import re
 import geoip2.database
 import geoip2.webservice
 from geoip2.errors import AddressNotFoundError
-from flask import current_app, _app_ctx_stack
-
-VALID_SERVICE_TYPES = [
-    'local',
-    'webservice',
-]
+from flask import current_app
 
 class GeoIP2(object):
     def __init__(self, app=None):
@@ -17,71 +12,72 @@ class GeoIP2(object):
             self.init_app(app)
 
     def init_app(self, app):
-        app.config.setdefault('MAXMIND_SERVICE_TYPE', 'local')
-        app.config.setdefault('MAXMIND_WEBSERVICE_ID', None)
-        app.config.setdefault('MAXMIND_WEBSERVICE_LICENSE', None)
-        app.config.setdefault('MAXMIND_DB_PATH', None)
+        app.config.setdefault('FLASK_MM_GEOIP2_WEBSERVICE_ID', None)
+        app.config.setdefault('FLASK_MM_GEOIP2_WEBSERVICE_LICENSE', None)
+        app.config.setdefault('FLASK_MM_GEOIP2_DB_PATH', None)
+        app.config.setdefault('FLASK_MM_GEOIP2_DEVELOPMENT_MODE', False)
+        app.config.setdefault('FLASK_MM_GEOIP2_DEVELOPMENT_IP', '1.1.1.1')
 
         # To save us some typing later...
-        self.service_type = app.config['MAXMIND_SERVICE_TYPE']
-        self.webservice_id = app.config['MAXMIND_WEBSERVICE_ID']
-        self.webservice_license = app.config['MAXMIND_WEBSERVICE_LICENSE']
-        self.db_path = app.config['MAXMIND_DB_PATH']
-
-        if self.service_type not in VALID_SERVICE_TYPES:
-            raise TypeError('\'{}\' is not a valid Flask-MM-GeoIP2 service type.'.format(self.service_type))
+        self.webservice_id = app.config['FLASK_MM_GEOIP2_WEBSERVICE_ID']
+        self.webservice_license = app.config['FLASK_MM_GEOIP2_WEBSERVICE_LICENSE']
+        self.db_path = app.config['FLASK_MM_GEOIP2_DB_PATH']
+        self.development_mode = app.config['FLASK_MM_GEOIP2_DEVELOPMENT_MODE']
+        self.development_ip = app.config['FLASK_MM_GEOIP2_DEVELOPMENT_MODE']
         
         self.app = app
     
-    def get_geoip_data(self, ip_address):
+    def get_local_geoip_data(self, ip_address):
         """
-        Performs a lookup from a MaxMind GeoIP database (either local or remote, based
-        on Flask app config settings).  Returns a geoip2 response object.
+        Performs a lookup from a local MaxMind GeoIP database.  Returns a geoip2 response object.
         """
-        if self.service_type == "local":
-            # Performing a lookup from a local DB
-            if self.db_path:
-                if os.path.isfile(self.db_path):
-                    reader = geoip2.database.Reader(self.db_path)
+        if self.db_path:
+            if os.path.isfile(self.db_path):
+                reader = geoip2.database.Reader(self.db_path)
 
-                    # Determine from the db_path what function to call.  MaxMind has
-                    # very generously correlated the function calls suppored by
-                    # the reader object to the names of the databases, so it's not 
-                    # hard to divine what call to make.
-                    p = re.compile('^.*\/?-(.*)\.mmdb$')
-                    m = p.match(self.db_path)
-                    lookup_function = m.group(1).lower().replace('-','_')
+                # Determine from the db_path what function to call.  MaxMind has
+                # very generously correlated the function calls suppored by
+                # the reader object to the names of the databases, so it's not 
+                # hard to divine what call to make.
+                p = re.compile('^.*\/?-(.*)\.mmdb$')
+                m = p.match(self.db_path)
+                lookup_function = m.group(1).lower().replace('-','_')
 
-                    try:
-                        # Using black magic to make this function call...
-                        # this is safe to do, right?
-                        result = getattr(reader, lookup_function)(ip_address)
-                    except AttributeError:
-                        raise ValueError('Unable to determine MaxMind database type - make sure you didn\'t rename the database.')
-                    except AddressNotFoundError:
-                        return None
-
-                    return result
-                else:
-                    raise FileNotFoundError('The MaxMind DB at {} could not be found.'.format(db_path))
-            else:
-                raise ValueError('The MAXMIND_DB_PATH configuration directive is not set.')
-
-        else:
-            # Performing an API call
-            if webservice_id and webservice_license:
                 try:
-                    client = geoip2.webservice.Client(self.webservice_id, self.webservice_license)
+                    # Using black magic to make this function call...
+                    # this is safe to do, right?
+                    if not self.development_mode:
+                        result = getattr(reader, lookup_function)(ip_address)
+                    else:
+                        result = getattr(reader, lookup_function)(self.development_ip)
+                except AttributeError:
+                    raise ValueError('Unable to determine MaxMind database type - make sure you didn\'t rename the database.')
                 except AddressNotFoundError:
                     return None
 
-                # TODO: There's a lot that can go wrong here (see geoip2.errors), 
-                # and there is probably a potential for something very stupid to 
-                # abend the Flask app.
-
-                # TODO: API calls are pinned to "city" since, unlike the local db
-                # method, we have no good way of determining whether we should use
-                # city or country to perform this lookup.  
-                return client.city(ip_address)
+                return result
             else:
-                raise TypeError('Either MAXMIND_WEBSERVICE_ID or MAXMIND_WEBSERVICE_LICENSE is not set.')
+                raise FileNotFoundError('The MaxMind DB at {} could not be found.'.format(db_path))
+        else:
+            raise ValueError('The FLASK_MM_GEOIP2_DB_PATH configuration directive is not set.')
+
+
+    def get_webservice_geoip_data(self, ip_address, query_type='city')
+        # Performing an API call
+        if webservice_id and webservice_license:
+                client = geoip2.webservice.Client(self.webservice_id, self.webservice_license)
+
+            # TODO: There's a lot that can go wrong here (see geoip2.errors), 
+            # and there is probably a potential for something very stupid to 
+            # abend the Flask app.
+            try:
+                if not self.development_mode:
+                    result = getattr(client, query_type)(ip_address)
+                else:
+                    result = getattr(client, query_type)(self.development_ip)
+            except AddressNotFoundError:
+                return None
+            
+            return result
+        else:
+            raise TypeError('Either FLASK_MM_GEOIP2_WEBSERVICE_ID or FLASK_MM_GEOIP2_WEBSERVICE_LICENSE is not set.')
